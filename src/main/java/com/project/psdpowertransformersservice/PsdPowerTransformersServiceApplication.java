3cbf08ecaaf54484310ca4eb365f237fddf5ebf3
EllsaG <seninlsa@gmail.com>
2024-07-07 00:43:24 +0400

    Added files

---
+++ src/main/java/com/project/psdpowertransformersservice/PsdPowerTransformersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.psdpowertransformersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsdPowerTransformersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsdPowerTransformersServiceApplication.class, args);
+	}
+
+}
